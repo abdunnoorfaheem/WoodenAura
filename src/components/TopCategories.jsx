ec46957435adfa05a36b56e5fc94815a71ebbec2
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-11-29 01:58:34 +0600

    Top Categories section added

--- src/components/TopCategories.jsx
+++ src/components/TopCategories.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useContext } from "react";
 import topChair from '../assets/topChair.png';
+import { apiData } from "./ContextApi";
 
 const TopCategories = () => {
+
+let data=useContext(apiData);
+
+console.log(data);
+
+let filterData=data.filter((item)=>item.id>=1 && item.id<=3)
+  
+    
   return (
     <section>
         <h2 className='text-2xl md:text-4xl text-[#151875] font-bold text-center'>Top Categories</h2>
-        <div className="container mx-auto">
-            
-            <div className='md:w-[24%] h-[345px]  shadow-2xl'>
-                <div className='flex justify-center py-6'>
-                    <img className='bg-[#F6F7FB] p-6 rounded-full ' src={topChair} alt="" />
+        <div className="container mx-auto md:flex justify-between mt-6">
+
+            {filterData.map((item)=>(
+                <div className='md:w-[32%] max-h-[645px] mb-8  shadow-2xl group'>
+                <div className='flex justify-center py-6 '>
+                    <img className='bg-[#F6F7FB] p-6 rounded-full group-hover:border-b-[10px] group-hover:border-purple-500 group-hover:rotate-90 duration-500 ease-in-out' src={item.thumbnail} alt="" />
                 </div>
                 <div className='text-[#151875] py-4 font-semibold'>
-                    <h4 className='text-center'>Mini LCW Chair</h4>
-                    <p className='text-center'>$56.00</p>
+                    <h4 className='text-center'>{item.title}</h4>
+                    <p className='text-center'>${item.price}</p>
                 </div>
             </div>
+            ))}
+            
+            
         </div>
     </section>
   )
