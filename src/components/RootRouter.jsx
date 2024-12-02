f77b2c27196a46154349d7c06bc4e92c6a377bec
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-12-02 22:51:20 +0600

    Header section added

--- src/components/RootRouter.jsx
+++ src/components/RootRouter.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import Header from './Header'
 
 const RootRouter = () => {
   return (
     <>
+    <Header/>
     <NavBar/>
     <Outlet/>
     <Footer/>
--- src/components/UniqueFeatured.jsx
+++ src/components/UniqueFeatured.jsx
@@ -10,10 +10,10 @@ const UniqueFeatured = () => {
         </div>
         <div className="md:w-[50%]">
           <h3 className="text-[#151875] text-xl md:text-3xl font-bold">Unique Features of latest & <br /> Trending Products</h3>
-          <ul className="list-disc text-[#ACABC3] mt-4">
-            <li>All frames constructed with hardwood solids and laminates</li>
-            <li>Reinforced with double wood dowels, glue, screw - nails corner  <br /> blocks and machine nails</li>
-            <li>Arms, backs and seats are structurally reinforced</li>
+          <ul className=" text-[#ACABC3] mt-4">
+            <li className="list-disc">All frames constructed with hardwood solids and laminates</li>
+            <li className="list-disc">Reinforced with double wood dowels, glue, screw - nails corner  <br /> blocks and machine nails</li>
+            <li className="list-disc">Arms, backs and seats are structurally reinforced</li>
           </ul>
           <div className="md:flex mt-5 gap-3">
             <button className="bg-[#FB2E86] text-white px-3 py-1 rounded-sm">Add to Cart</button>
